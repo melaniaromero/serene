da5377bebdf2d4842d0b700d9f8737e1a8300404
francesca <melaniapedraza17@gmail.com>
2023-05-21 18:45:23 -0600

    Cambios algoritmo

---
+++ production/py/a1_asociacion.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import csv
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+
+def index ():
+    return render_template('a1_asociacion.html')
+
+@app.route('/data', methods=['GET', 'POST'])
+
+def data():
+    if request.method == 'POST':
+        f = request.form['csvfile']
+        data = []
+        with open(f) as file:
+            csvfile = csv.reader(file)
+            for row in csvfile:
+                data.append(row)
+        return render_template('r_a1_asociacion.html' , data=data)
+                         
+
+if __name__ == '__main__':
+    app.run(debug=True)
